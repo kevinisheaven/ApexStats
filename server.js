9f6edb736933e00a6fe9ac63ae44ee92e0f5fbbe
Kevin Horwitz <khorwitz1992@gmail.com>
2022-09-26 18:39:28 -0500

    commenting out production

--- server.js
+++ server.js
@@ -24,15 +24,15 @@ if(process.env.NODE_ENV === 'development') {
 app.use('/api/v1/profile', profile)
 
 // Handle production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static(__dirname + '/public'))
+// if (process.env.NODE_ENV === 'production') {
+//     // Set static folder
+//     app.use(express.static(__dirname + '/public/'))
 
-    // Handle SPA (single page application)
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
-}
+//     // Handle SPA (single page application)
+//     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+// }
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 5003 //|| 8000
 
 
 app.listen(port, () => {
